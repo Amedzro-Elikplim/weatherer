fefd743fa600e71c5552a68ef65804ebeecbfaa6
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2020-11-05 19:31:38 +0000

    initial commit

---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "../Styles/Home.css";
+import "bootstrap";
+const Home = (props) => {
+
+    
+    return (
+        <div className = "container-fluid Home">
+            <div className = "row">
+                
+            </div>
+        </div>
+    );
+};
+
+ 
+
+export default Home;
